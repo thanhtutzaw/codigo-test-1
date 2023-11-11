f0c9c82e477d08be62e118d1d349b2b7e448947f
thanhtutzaw <zgyi5025@gmail.com>
2023-11-11 13:02:02 +0630

    layout

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['nft.fluffyhugs.io', 'thz.vercel.app'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'nft.fluffyhugs.io',
+        port: '',
+        pathname: '/',
+      },
+    ],
+  }
 }
 
 module.exports = nextConfig
