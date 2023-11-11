ccd45bf22642694597bdebbae343c7b72f542e25
thanhtutzaw <zgyi5025@gmail.com>
2023-11-11 17:35:32 +0630

    animate position

--- src/Components/PageLoader.jsx
+++ src/Components/PageLoader.jsx
@@ -2,13 +2,23 @@ import Image from "next/image";
 import s from "@/styles/Home.module.scss";
 export default function PageLoader({ pageLoading }) {
   return (
-    <div className={s.loading} data-loaded={!pageLoading}>
-      <Image
-        width={200}
-        height={200}
-        alt="loading"
-        src={"https://nft.fluffyhugs.io/loading/loading.webp"}
-      />
-    </div>
+    <>
+      <div
+        style={{ display: "flex", flexDirection: "column" }}
+        className={s.loading}
+        data-loaded={!pageLoading}
+      >
+        <Image
+          width={200}
+          height={200}
+          alt="loading"
+          src={"https://nft.fluffyhugs.io/loading/loading.webp"}
+        />
+        <p style={{ position: "absoulte", bottom: "0" }}>
+          Sorry ! This is all I can do . In my low spec pc and without three js
+          knowledge in 1 day.
+        </p>
+      </div>
+    </>
   );
 }
