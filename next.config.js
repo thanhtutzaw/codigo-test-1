a99e1bc50aeb34b4b2f102705647e55a75ec5a4d
thanhtutzaw <zgyi5025@gmail.com>
2023-11-11 13:43:19 +0630

    fix : img hostname

--- next.config.js
+++ next.config.js
@@ -2,13 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['nft.fluffyhugs.io', 'thz.vercel.app'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'nft.fluffyhugs.io',
         port: '',
-        pathname: '/',
       },
     ],
   }
